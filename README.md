067bfaac8dac44696510fdc7e43ab62cd14c02d7
Vedant Agrawal <vedantagrawal524@gamil.com>
2025-05-21 11:48:44 +0530

    Project Setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Mortgage repayment calculator
+
+- Lime: hsl(61, 70%, 52%)
+- Red: hsl(4, 69%, 50%)
+
+- Slate 100: hsl(202, 86%, 94%)
+- Slate 300: hsl(203, 41%, 72%)
+- Slate 500: hsl(200, 26%, 54%)
+- Slate 700: hsl(200, 24%, 40%)
+- Slate 900: hsl(202, 55%, 16%)
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <>
+      <div>
+        Mortgage Calculator Clear All Mortgage Amount Mortgage Term Interest
+        Rate Mortgage Type Repayment Interest Only Calculate Repayments Results
+        shown here Complete the form and click “calculate repayments” to see
+        what your monthly repayments would be. Your results Your results are
+        shown below based on the information you provided. To adjust the
+        results, edit the form and click “calculate repayments” again. Your
+        monthly repayments Total you'll repay over the term
+      </div>
+    </>
+  );
+}
