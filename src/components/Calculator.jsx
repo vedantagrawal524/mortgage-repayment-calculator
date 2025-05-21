a067c6092fadfb3c99e54af8c670eeea2051d8de
Vedant Agrawal <vedantagrawal524@gamil.com>
2025-05-22 00:21:45 +0530

    UI

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -17,7 +17,7 @@ export default function Calculator() {
           </button>
         </div>
 
-        <fieldset>
+        <fieldset className="inputFieldset error">
           <label
             htmlFor="mortageAmount"
             className="text-sslate-700 text-[0.82rem] font-[500]"
@@ -31,17 +31,21 @@ export default function Calculator() {
               id="mortageAmount"
               name="mortageAmount"
               min={0}
-              className="outline-sslate-500 hover:outline-sslate-900 active:outline-new-lime focus:outline-new-lime text-sslate-900 peer w-full rounded-[0.3rem] border-0 py-2 pr-1 pl-11 text-[0.9rem] font-[700] outline-[1.5px] hover:cursor-pointer focus:outline-2 active:outline-2"
+              className="outline-sslate-500 hover:outline-sslate-900 active:outline-new-lime focus:outline-new-lime text-sslate-900 peer error inputField relative w-full rounded-[0.3rem] border-0 py-2 pr-1 pl-11 text-[0.9rem] font-[700] outline-[1.5px] hover:cursor-pointer focus:outline-2 active:outline-2"
             />
 
-            <div className="bg-sslate-100 text-sslate-700 peer-active:bg-new-lime peer-active:text-sslate-900 peer-focus:bg-new-lime peer-focus:text-sslate-900 absolute bottom-0 left-0 flex h-full w-9 items-center justify-center rounded-tl-[0.3rem] rounded-bl-[0.3rem] p-2 text-[0.85rem] font-[600]">
+            <div className="bg-sslate-100 text-sslate-700 peer-active:bg-new-lime peer-active:text-sslate-900 peer-focus:bg-new-lime peer-focus:text-sslate-900 error errorIcon absolute bottom-0 left-0 flex h-full w-9 items-center justify-center rounded-tl-[0.3rem] rounded-bl-[0.3rem] p-2 text-[0.85rem] font-[600]">
               £
+            </div>
+
+            <div className="text-new-red errorMsg error absolute bottom-[-1.05rem] left-0 z-20 hidden text-[0.65rem] font-[600]">
+              This field is required
             </div>
           </div>
         </fieldset>
 
-        <div className="xs:flex-row flex flex-col gap-2">
-          <fieldset>
+        <div className="xs:flex-row xs:gap-3.5 mt-2 flex flex-col gap-3">
+          <fieldset className="inputFieldset error">
             <label
               htmlFor="mortageTerm"
               className="text-sslate-700 text-[0.82rem] font-[500]"
@@ -55,16 +59,20 @@ export default function Calculator() {
                 id="mortageTerm"
                 name="mortageTerm"
                 min={0}
-                className="outline-sslate-500 hover:outline-sslate-900 active:outline-new-lime focus:outline-new-lime text-sslate-900 peer w-full rounded-[0.3rem] border-0 py-2 pr-[3.7rem] pl-2 text-[0.9rem] font-[700] outline-[1.5px] hover:cursor-pointer focus:outline-2 active:outline-2"
+                className="outline-sslate-500 hover:outline-sslate-900 active:outline-new-lime focus:outline-new-lime text-sslate-900 peer inputField error w-full rounded-[0.3rem] border-0 py-2 pr-[3.7rem] pl-2 text-[0.9rem] font-[700] outline-[1.5px] hover:cursor-pointer focus:outline-2 active:outline-2"
               />
 
-              <div className="bg-sslate-100 text-sslate-700 peer-active:bg-new-lime peer-active:text-sslate-900 peer-focus:bg-new-lime peer-focus:text-sslate-900 absolute right-0 bottom-0 flex h-full w-14 items-center justify-center rounded-tr-[0.3rem] rounded-br-[0.3rem] p-2 text-[0.85rem] font-[600]">
+              <div className="bg-sslate-100 text-sslate-700 peer-active:bg-new-lime peer-active:text-sslate-900 peer-focus:bg-new-lime peer-focus:text-sslate-900 errorIcon error absolute right-0 bottom-0 flex h-full w-14 items-center justify-center rounded-tr-[0.3rem] rounded-br-[0.3rem] p-2 text-[0.85rem] font-[600]">
                 years
+              </div>
+
+              <div className="text-new-red errorMsg error absolute bottom-[-1.05rem] left-0 z-20 hidden text-[0.65rem] font-[600]">
+                This field is required
               </div>
             </div>
           </fieldset>
 
-          <fieldset>
+          <fieldset className="inputFieldset error">
             <label
               htmlFor="interestRate"
               className="text-sslate-700 text-[0.82rem] font-[500]"
@@ -78,22 +86,26 @@ export default function Calculator() {
                 id="interestRate"
                 name="interestRate"
                 min={0}
-                className="outline-sslate-500 hover:outline-sslate-900 active:outline-new-lime focus:outline-new-lime text-sslate-900 peer w-full rounded-[0.3rem] border-0 py-2 pr-[3.7rem] pl-2 text-[0.9rem] font-[700] outline-[1.5px] hover:cursor-pointer focus:outline-2 active:outline-2"
+                className="outline-sslate-500 hover:outline-sslate-900 active:outline-new-lime focus:outline-new-lime text-sslate-900 peer inputField error w-full rounded-[0.3rem] border-0 py-2 pr-[3.7rem] pl-2 text-[0.9rem] font-[700] outline-[1.5px] hover:cursor-pointer focus:outline-2 active:outline-2"
               />
 
-              <div className="bg-sslate-100 text-sslate-700 peer-active:bg-new-lime peer-active:text-sslate-900 peer-focus:bg-new-lime peer-focus:text-sslate-900 absolute right-0 bottom-0 flex h-full w-9 items-center justify-center rounded-tr-[0.3rem] rounded-br-[0.3rem] p-2 text-[0.85rem] font-[600]">
+              <div className="bg-sslate-100 text-sslate-700 peer-active:bg-new-lime peer-active:text-sslate-900 peer-focus:bg-new-lime peer-focus:text-sslate-900 errorIcon error absolute right-0 bottom-0 flex h-full w-9 items-center justify-center rounded-tr-[0.3rem] rounded-br-[0.3rem] p-2 text-[0.85rem] font-[600]">
                 %
+              </div>
+
+              <div className="text-new-red errorMsg error absolute bottom-[-1.05rem] left-0 z-20 hidden text-[0.65rem] font-[600]">
+                This field is required
               </div>
             </div>
           </fieldset>
         </div>
 
-        <fieldset>
+        <fieldset className="inputFieldset error relative mt-3 rounded-[0.3rem]">
           <legend className="text-sslate-700 text-[0.82rem] font-[500]">
             Mortgage Type
           </legend>
 
-          <div className="relative mt-2">
+          <div className="relative mt-1">
             <input
               type="radio"
               id="repayment"
@@ -125,6 +137,10 @@ export default function Calculator() {
               Interest Only
             </label>
             <span className="peer-checked:bg-new-lime peer-checked:ring-new-lime absolute top-3 left-4 h-2.5 w-2.5 rounded-full ring-1 ring-slate-700 peer-checked:ring-offset-2"></span>
+          </div>
+
+          <div className="text-new-red errorMsg error absolute bottom-[-1.05rem] left-0 z-20 hidden text-[0.65rem] font-[600]">
+            This field is required
           </div>
         </fieldset>
 
